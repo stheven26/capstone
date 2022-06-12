405211426a8aea4de700613ce8ce45ac1a04ad52
Stheven Erlangga Sanjaya <sthevenerlanggasanjaya@gmail.com>
2022-06-12 12:29:01 +0700

    added new file

--- main.py
+++ main.py
@@ -1,51 +1,58 @@
 # import library
 from flask import Flask, request, make_response, jsonify, redirect, url_for
-from flask_restful import Resource, Api 
+from flask_restful import Resource, Api
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 import bcrypt
-import jwt 
-import os 
-import datetime 
+import jwt
+import os
+import datetime
 
-#initialization
+# initialization
 app = Flask(__name__)
 api = Api(app)
 db = SQLAlchemy(app)
 CORS(app)
 
-# configure database ==> create file db.sqlite 
+# configure database ==> create file db.sqlite
 filename = os.path.dirname(os.path.abspath(__file__))
 database = 'sqlite:///' + os.path.join(filename, 'db.sqlite')
 app.config['SQLALCHEMY_DATABASE_URI'] = database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# configure secret key 
+# configure secret key
 app.config['SECRET_KEY'] = "thisissecretkey"
 
 # create schema model database authentication (login, register)
+
+
 class AuthModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
+
 # create database
 db.create_all()
 
 # create decorator
+
+
 def token(f):
     @wraps(f)
     def decorator(*args, **kwargs):
-        token = request.args.get('token') # http://127.0.0.1:5000/api/v1/profile/id?token=jwttoken
+        # http://127.0.0.1:5000/api/v1/profile/id?token=jwttoken
+        token = request.args.get('token')
         if not token:
-            return make_response(jsonify({"msg":"no have token!"}), 401)
+            return make_response(jsonify({"msg": "no have token!"}), 401)
         try:
             jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
         except:
-            return make_response(jsonify({"msg":"invalid token!"}), 401)
+            return make_response(jsonify({"msg": "invalid token!"}), 401)
         return f(*args, **kwargs)
     return decorator
+
 
 class Register(Resource):
     def post(self):
@@ -55,14 +62,14 @@ class Register(Resource):
 
         # check that email and password exist
         if not dataEmail:
-            return make_response(jsonify({"msg":"email cannot be empty"}), 400)
+            return make_response(jsonify({"msg": "email cannot be empty"}), 400)
         if not dataPassword:
-            return make_response(jsonify({"msg":"password cannot be empty"}), 400)
+            return make_response(jsonify({"msg": "password cannot be empty"}), 400)
         if dataEmail and hashed:
             dataModel = AuthModel(email=dataEmail, password=hashed)
             db.session.add(dataModel)
             db.session.commit()
-            return make_response(jsonify({"msg":"successful registration"}), 200)
+            return make_response(jsonify({"msg": "successful registration"}), 200)
 
 
 # routing login authentication
@@ -75,25 +82,26 @@ class Login(Resource):
         queryEmail = [data.email for data in AuthModel.query.all()]
         user = AuthModel.query.filter_by(email=dataEmail).first()
         if not dataEmail:
-            return make_response(jsonify({"msg":"email cannot be empty"}), 400)
+            return make_response(jsonify({"msg": "email cannot be empty"}), 400)
         if not dataPassword:
-            return make_response(jsonify({"msg":"password cannot be empty"}), 400)
+            return make_response(jsonify({"msg": "password cannot be empty"}), 400)
         if not user:
-            return make_response(jsonify({"msg":"user not found"}), 404)
+            return make_response(jsonify({"msg": "user not found"}), 404)
         if bcrypt.checkpw(dataPassword.encode("utf-8"), user.password):
             token = jwt.encode(
                 {
-                    "email":queryEmail, 
-                    "exp":datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
+                    "email": queryEmail,
+                    "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
                 }, app.config['SECRET_KEY'],  algorithm="HS256"
             )
             return make_response(jsonify(
                 {
-                    "msg":"successful login", 
-                    "token":token
-                    }), 200)
+                    "msg": "successful login",
+                    "token": token
+                }), 200)
         else:
-            return jsonify({"msg":"Login failed, please try again!"})
+            return jsonify({"msg": "Login failed, please try again!"})
+
 
 class Profile(Resource):
     @token
@@ -101,17 +109,18 @@ class Profile(Resource):
         dataQuery = AuthModel.query.get(id)
         output = [
             {
-                "id":dataQuery.id,
+                "id": dataQuery.id,
                 "email": dataQuery.email,
-            } 
-        ] 
+            }
+        ]
         return make_response(jsonify(output), 200)
 
-#add api
+
+# add api
 api.add_resource(Register, "/api/v1/register", methods=["POST"])
 api.add_resource(Login, "/api/v1/login", methods=["POST"])
 api.add_resource(Profile, "/api/v1/profile/<id>", methods=["GET"])
 
-#run app
+# run app
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
